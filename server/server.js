bae09cce016648c933a44b359ff5497cdfa7ef6b
Laertes <maclellanj15@gmail.com>
2022-05-23 15:11:14 -0500

    restructured auth middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
+
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+const auth = require('./utils/auth');
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +28,20 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Apollo server instance
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);      
+    })
+  });
+};
+
+// Call async function to start server
+startApolloServer(typeDefs, resolvers);
+
