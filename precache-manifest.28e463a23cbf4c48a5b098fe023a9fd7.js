be8d954369e603077c9da356e3693ec0cdd64cca
Laertes <maclellanj15@gmail.com>
2022-05-25 02:46:02 -0500

    Updates

---
+++ precache-manifest.28e463a23cbf4c48a5b098fe023a9fd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80d7e2fb35a6099cda58353d6b4d56d",
+    "url": "/lookup-book/index.html"
+  },
+  {
+    "revision": "234f2c06d1c7194c241d",
+    "url": "/lookup-book/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7e6f384410b9163433da",
+    "url": "/lookup-book/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "234f2c06d1c7194c241d",
+    "url": "/lookup-book/static/js/2.5567bace.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lookup-book/static/js/2.5567bace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6f384410b9163433da",
+    "url": "/lookup-book/static/js/main.4606b7c5.chunk.js"
+  },
+  {
+    "revision": "bec8a706f4e795176661",
+    "url": "/lookup-book/static/js/runtime-main.c29b51e9.js"
+  }
+]);
